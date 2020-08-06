86b22bbae06d2033d77840eff96c884410eb0ace
Muyoba Lutlas <lutakachihota@gmail.com>
2020-08-06 06:15:44 +0200

    Initial commit

---
+++ app/src/main/java/io/eresawsaltul/cardview/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.eresawsaltul.cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+    }
+}
